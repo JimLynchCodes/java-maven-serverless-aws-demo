c7437f421bc3cc2e5ccc58639a38378408390e2a
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-10-18 11:06:36 -0400

    modified pom

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -16,7 +16,7 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: {}", input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!!", input);
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
---
+++ src/main/java/com/serverless/HandlerTest.java
@@ -0,0 +1,20 @@
+package com.serverless;
+
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class HandlerTest {
+
+//   String message = "Robert";	
+//   MessageUtil messageUtil = new MessageUtil(message);
+ 
+   @Test
+   public void testSalutationMessage() {
+      System.out.println("Inside testSalutationMessage()");
+      
+//      message = "Hz" + "Robert";
+//      assertEquals(true, true);
+   }
+}
